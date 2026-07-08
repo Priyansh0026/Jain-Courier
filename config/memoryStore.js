367089f60b0dd0a76f8f9f5ef3edde57eac105dd
Priyansh0026 <tjain0911@gmail.com>
2026-07-08 13:20:01 +0530

    Delete config directory

--- config/memoryStore.js
+++
@@ -1,7 +0,0 @@
-// In-memory fallback database store to allow testing without local MongoDB installation
-const memoryStore = {
-  users: [],
-  verifications: new Map()
-};
-
-module.exports = memoryStore;
